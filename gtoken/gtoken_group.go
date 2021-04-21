bd328afc4e6bcbe139f383981135c5eac9aac6b1
kingyond <900483@qq.com>
2021-04-21 22:05:56 +0800

    增加绑定group,不增加route函数MiddlewareEx

--- gtoken/gtoken_group.go
+++ gtoken/gtoken_group.go
@@ -36,3 +36,33 @@ func (m *GfToken) Middleware(group *ghttp.RouterGroup) bool {
 
 	return true
 }
+
+// MiddlewareEx 绑定group,不增加route
+func (m *GfToken) MiddlewareEx(group *ghttp.RouterGroup) bool {
+	if !m.InitConfig() {
+		return false
+	}
+	// 设置为Group模式
+	m.MiddlewareType = MiddlewareTypeGroup
+	glog.Info("[GToken][params:" + m.String() + "]start... ")
+
+	// 缓存模式
+	if m.CacheMode > CacheModeRedis {
+		glog.Error("[GToken]CacheMode set error")
+		return false
+	}
+	// 登录
+	if m.LoginPath == "" || m.LoginBeforeFunc == nil {
+		glog.Error("[GToken]LoginPath or LoginBeforeFunc not set")
+		return false
+	}
+	// 登出
+	if m.LogoutPath == "" {
+		glog.Error("[GToken]LogoutPath or LogoutFunc not set")
+		return false
+	}
+
+	group.Middleware(m.authMiddleware)
+
+	return true
+}
